e2de692e58551504d538882fb8dd62bf28e2e909
Robin Deits <robin.deits@gmail.com>
2013-04-07 21:06:42 -0400

    Organize proposals; count number of multiplications in xcorr_fp

--- src/matlab_fp/xcorr_fp.m
+++ src/matlab_fp/xcorr_fp.m
@@ -1,6 +1,7 @@
 function C = xcorr_fp(A, B)
   assert(size(A, 1) == size(B, 1))
   assert(size(A, 2) == size(B, 2))
+  muls = 0;
 
   C = zeros(size(A, 1) + size(B, 1) - 1, size(A, 2) + size(B, 2) - 1);
   for i = 1:(size(C, 1))
@@ -11,10 +12,12 @@ function C = xcorr_fp(A, B)
           na = n + j - size(B, 2);
           if (ma <= size(A, 1)) && (na <= size(A, 2)) && ma > 0 && na > 0
             C(i, j) = C(i, j) + A(ma, na) * B(m, n);
+            muls = muls+1;
           end
         end
       end
     end
   end
+  muls
 end
 
