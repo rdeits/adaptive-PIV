8eac71dd892d81e3f4e8791351a307d8e1162190
Robin Deits <robin.deits@gmail.com>
2013-04-07 22:32:27 -0400

    Started microarch writeup

--- src/matlab_fp/xcorr_fp.m
+++ src/matlab_fp/xcorr_fp.m
@@ -1,9 +1,9 @@
 function C = xcorr_fp(A, B)
-  assert(size(A, 1) == size(B, 1))
-  assert(size(A, 2) == size(B, 2))
+  assert(size(A, 1) > size(B, 1))
+  assert(size(A, 2) > size(B, 2))
   muls = 0;
 
-  C = zeros(size(A, 1) + size(B, 1) - 1, size(A, 2) + size(B, 2) - 1);
+  C = zeros(size(A, 1) - size(B, 1) + 1, size(A, 2) - size(B, 2) + 1);
   for i = 1:(size(C, 1))
     for j = 1:(size(C, 2))
       for m = 1:(size(A, 1))
