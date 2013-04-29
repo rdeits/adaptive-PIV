4b95ccf06b3ec51cb93df18196677664b7c34aa2
Robin Deits <robin.deits@gmail.com>
2013-04-28 23:49:32 -0400

    switch to big images

--- src/pyPIV/config.py
+++ src/pyPIV/config.py
@@ -3,13 +3,13 @@ import os
 test_crop = [0, 0, 40, 40]
 
 window_spacing = 8
-im_width = 80
-im_height = 60
+im_width = 800
+im_height = 600
 frame_size = 40
 frame_cols = (im_width - frame_size) // window_spacing + 1
 frame_rows = (im_height - frame_size) // window_spacing + 1
 
-source_pairs = [('A.png', 'B.png')]
+source_pairs = [('A.tif', 'B.tif')]
 # image_dir = '../../data/vort_sim/'
 # source_pairs = [('single_vort_sim_0001.tif', 'single_vort_sim_0002.tif')]
 
